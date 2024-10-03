5f51ac4d7776e2b22ce43d126fb33a2735b5155a
quangvansuong <quangvansuong.2014485@gmail.com>
2024-10-03 11:48:36 +0700

    Create CardsItem

--- travel_website/src/components/Button.js
+++ travel_website/src/components/Button.js
@@ -6,7 +6,7 @@ const STYLES = ['btn--primary', 'btn--outline'];
 const SIZES = ['btn--medium', 'btn--large'];
 
 
-export const Button =({
+export const Button = ({
     children,
     type,
     onClick,
@@ -14,19 +14,19 @@ export const Button =({
     buttonSize
 }) => {
     const checkButtonStyle = STYLES.includes(buttonStyle)
-    ? buttonStyle
-    : STYLES[0];
+        ? buttonStyle
+        : STYLES[0];
     const checkButtonSize = SIZES.includes(buttonSize)
-    ? buttonSize
-    : SIZES[0];
+        ? buttonSize
+        : SIZES[0];
 
-    return(
+    return (
         <Link to='/sign-up' className='btn-mobile'>
-            <button className={'btn ${checkButtonStyle} ${checkButtonSize}'} 
-            onClick={onClick}
-            type = {type}
+            <button className={'btn ${checkButtonStyle} ${checkButtonSize}'}
+                onClick={onClick}
+                type={type}
             >
-            {children}
+                {children}
             </button>
 
         </Link>
